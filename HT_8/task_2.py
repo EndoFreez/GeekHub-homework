2936856e234f67c921e38db3843bf5ea01ad4bc0
EndoFreez <freezy1991@gmail.com>
2022-11-03 17:50:39 +0200

    HT-8 reworked

--- HT_8/task_2.py
+++ HT_8/task_2.py
@@ -11,18 +11,29 @@
 
 def blocks(path, num):
 
-	try:
-	    with open(path, 'r') as i:
-	        text = i.read()
-	        if len(text) >= num:
-	            start = text[0:num]
-	            mid = text[(len(text) - num) // 2:(len(text) + num) // 2]
-	            end = text[-num:]
-	            return print(start, mid, end)
-	        else:
-	            return print("Your file is shorter than your option")
-	except FileNotFoundError:
-		print(f"File {path} not found")
+    try:
+        with open(path, 'r') as i:
+            text = i.read()
+            if num == 0:
+                return print(text)
+            if num < 0 and len(text) >= -num:
+                text = text[::-1]
+                num = -num
+                start = text[0:num]
+                mid = text[(len(text) - num) // 2:(len(text) + num) // 2]
+                end = text[-num:]
+                return print(start, mid, end)
+            if len(text) >= num:
+                start = text[0:num]
+                mid = text[(len(text) - num) // 2:(len(text) + num) // 2]
+                end = text[-num:]
+                return print(start, mid, end)
+            else:
+                return print("Your file is shorter than your option")
+    except ZeroDivisionError:
+        print("Can't use Zero")
+    except FileNotFoundError:
+        print(f"File {path} not found")
 
 if __name__ == "__main__":
-    blocks("task_2.txt", 6)
+    blocks("task_2.txt", -5), blocks("task_2.txt", 5)
--- HT_8/task_3/ATM.py
+++ HT_8/task_3/ATM.py
@@ -5,72 +5,71 @@ import csv
 import json
 
 
+def login(user, password):
+
+    try:
+        with open("users.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row["User"] == user and row["Password"] == password:
+                    return True
+            return False
+    except FileNotFoundError:
+        print("Something wrong!@#$%")
+
+
+def check_balance(user):
+
+    with open(user + "_balance.txt", 'r') as balance:
+        return balance.read()
+
+
+def replenish(user):
+
+    try:
+        deposit = int(input("Deposit amount: "))
+        with open(user + "_balance.txt", "r+") as file:
+            lines = file.readlines()
+
+            if deposit > 0:
+                new_bal = int(lines[0]) + deposit
+                print(f'Your new balance is {new_bal}.')
+                file.seek(0)
+                file.write(str(new_bal))
+                return new_bal
+            else:
+                print("You can't do this!")
+
+    except FileNotFoundError:
+        print("You have no balance at our bank!") 
+    except ValueError:
+        print("Incorrect amount")
+
+
+def withdraw(user):
+
+    try:
+        money = int(input("Enter the amount: "))
+        balance = int(check_balance(user))
+        if money <= 0:
+            print("You can't do this")
+            return print(balance)
+        if money > balance:
+            print("You don't have enough funds")
+            return f"Try again, you have {balance}"
+        with open(user + "_balance.txt", 'r+') as file:
+            file.write(str(balance - money))
+        transaction = {"withdrawn funds" : money}
+        with open(user + "_transactions.json", "a") as t_file:
+            json.dump(transaction,t_file)
+        return print(f"You did it, now you have {check_balance(user)}")
+
+    except ValueError:
+        print("Incorrect amount")
+
+
 def start():
-
-    def login(user, password):
-
-        try:
-            with open("users.csv", "r") as file:
-                reader = csv.DictReader(file)
-                for row in reader:
-                    if row["User"] == user and row["Password"] == password:
-                        return True
-                return False
-        except FileNotFoundError:
-            print("Something wrong!@#$%")
-
-
-    def check_balance(user):
-
-        with open(user + "_balance.txt", 'r') as balance:
-            return balance.read()
-
-
-    def replenish(user):
-
-        try:
-            deposit = int(input("Deposit amount: "))
-            with open(user + "_balance.txt", "r+") as file:
-                lines = file.readlines()
-
-                if deposit > 0:
-                    new_bal = int(lines[0]) + deposit
-                    print(f'Your new balance is {new_bal}.')
-                    file.seek(0)
-                    file.write(str(new_bal))
-                    return new_bal
-                else:
-                    print("You can't do this!")
-
-        except FileNotFoundError:
-            print("You have no balance at our bank!") 
-        except ValueError:
-            print("Incorrect amount")
-
-
-    def withdraw(user):
-
-        try:
-            money = int(input("Enter the amount: "))
-            balance = int(check_balance(user))
-            if money <= 0:
-                print("You can't do this")
-                return print(balance)
-            if money > balance:
-                print("You don't have enough funds")
-                return f"Try again, you have {balance}"
-            with open(user + "_balance.txt", 'r+') as file:
-                file.write(str(balance - money))
-            transaction = {"withdrawn funds" : money}
-            with open(user + "_transactions.json", "a") as t_file:
-                json.dump(transaction,t_file)
-            return print(f"You did it, now you have {check_balance(user)}")
-
-        except ValueError:
-            print("Incorrect amount")
-
-
-        
+    
     user = input("Username: ")
     password = input("Password: ")
 
